6afe07f25b7718902b390efef292895f0809e137
gabrielolvmd <gabrielolvmd@gmail.com>
2023-06-30 14:50:14 -0300

    objects2-commit

--- JavaScript/Objetos/objetos_customizados.js
+++ JavaScript/Objetos/objetos_customizados.js
@@ -1,30 +1,32 @@
 function changeName(name) {
-    this.name = name
-    this.updatedAt = new Date()
+  this.name = name;
+  this.updatedAt = new Date();
 }
 
 const task1 = {
-    name: "task 1",
-    createdAt: new Date(),
-    updatedAt: null,
-    completed: false,
-    changeName
-}
+  name: 'task 1',
+  createdAt: new Date(),
+  updatedAt: null,
+  completed: false,
+  // changeName: changeName
+  changeName,
+};
 
 const task2 = {
-    name: "task 2",
-    createdAt: new Date(),
-    updatedAt: null,
-    completed: false,
-    changeName
-}
+  name: 'task 2',
+  createdAt: new Date(),
+  updatedAt: null,
+  completed: false,
+  // changeName: changeName
+  changeName,
+};
 
-task1.name = "task 1 updated"
-task1.updatedAt = new Date()
+task1.name = 'task 1 updated';
+task1.updatedAt = new Date();
 
-task1.changeName("nome atualizado")
+task1.changeName('nome atualizado');
 
-task2.changeName("novo nome")
+task2.changeName('novo nome');
 
 // console.log(task1)
-console.log(task2)
+console.log(task2);
