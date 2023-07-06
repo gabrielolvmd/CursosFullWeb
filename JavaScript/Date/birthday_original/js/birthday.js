4f23ab6b0239fe329799c2b3de3840bf59fa3659
gabrielolvmd <gabrielolvmd@gmail.com>
2023-07-06 01:19:06 -0300

    birthday-commit

--- JavaScript/Date/birthday_original/js/birthday.js
+++ JavaScript/Date/birthday_original/js/birthday.js
@@ -1,4 +1,7 @@
 function quantoFaltaPara(m, d) {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDay = currentDate.getDate();
 
-
+  return;
 }
